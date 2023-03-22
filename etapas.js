c0e37399b183229669aa6211d22f0975cc3078a9
droLz1n <maiapdr99@gmail.com>
2023-03-22 20:14:37 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome:'Candidato 1',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'vereador1.png', legenda:'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Candidato 2',
+                partido: 'XYZ',
+                fotos: [
+                    {url: 'vereador2.png', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Candidato 1',
+                partido: 'ABC',
+                vice: 'Vice 1',
+                fotos: [
+                    {url: 'prefeito1.png', legenda: 'Prefeito'},
+                    {url: 'prefeito1-2.png', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Candidato 2',
+                partido: 'XYZ',
+                vice: 'Vice 2',
+                fotos: [
+                    {url: 'prefeito2.png', legenda: 'Prefeito'},
+                    {url: 'prefeito2-2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
